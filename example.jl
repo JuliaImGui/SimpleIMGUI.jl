fcd1eec5f5e5ebbd0a9044d8ba536ab932ee2979
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2022-03-24 19:06:00 +0530

    render triangle to screen using GLFW (#1)
    * add GLFW and ModernGL packages
    
    * render triangle to screen

---
+++ example.jl
@@ -0,0 +1,125 @@
+import ModernGL as MGL
+import DataStructures as DS
+import GLFW
+import SimpleDraw as SD
+
+function process_input(window)
+    if GLFW.GetKey(window, GLFW.KEY_Q)
+        GLFW.SetWindowShouldClose(window, true)
+    end
+
+    return nothing
+end
+
+GLFW.WindowHint(GLFW.CONTEXT_VERSION_MAJOR, 3)
+GLFW.WindowHint(GLFW.CONTEXT_VERSION_MINOR, 3)
+GLFW.WindowHint(GLFW.OPENGL_PROFILE, GLFW.OPENGL_CORE_PROFILE)
+
+height_image = 600
+width_image = 800
+window_name = "Example"
+
+window = GLFW.CreateWindow(width_image, height_image, window_name)
+
+GLFW.MakeContextCurrent(window)
+
+renderer = unsafe_string(MGL.glGetString(MGL.GL_RENDERER))
+version = unsafe_string(MGL.glGetString(MGL.GL_VERSION))
+@info "Renderer: $(renderer)"
+@info "OpenGL version: $(version)"
+
+MGL.glViewport(0, 0, width_image, height_image)
+
+# vertex shader
+vertex_shader_source =
+"#version 330 core
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}"
+vertex_shader = MGL.glCreateShader(MGL.GL_VERTEX_SHADER)
+MGL.glShaderSource(vertex_shader, 1, Ptr{MGL.GLchar}[pointer(vertex_shader_source)], C_NULL)
+MGL.glCompileShader(vertex_shader)
+vertex_shader_success_ref = Ref{MGL.GLint}(0)
+MGL.glGetShaderiv(vertex_shader, MGL.GL_COMPILE_STATUS, vertex_shader_success_ref)
+@show vertex_shader_success_ref[]
+
+# fragment shader
+fragment_shader_source =
+"#version 330 core
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}"
+fragment_shader = MGL.glCreateShader(MGL.GL_FRAGMENT_SHADER)
+MGL.glShaderSource(fragment_shader, 1, Ptr{MGL.GLchar}[pointer(fragment_shader_source)], C_NULL)
+MGL.glCompileShader(fragment_shader)
+fragment_shader_success_ref = Ref{MGL.GLint}(0)
+MGL.glGetShaderiv(fragment_shader, MGL.GL_COMPILE_STATUS, fragment_shader_success_ref)
+@show fragment_shader_success_ref[]
+
+# shader program
+fragment_shader_source =
+shader_program = MGL.glCreateProgram()
+MGL.glAttachShader(shader_program, vertex_shader)
+MGL.glAttachShader(shader_program, fragment_shader)
+MGL.glLinkProgram(shader_program)
+shader_program_success_ref = Ref{MGL.GLint}(0)
+MGL.glGetProgramiv(shader_program, MGL.GL_LINK_STATUS, shader_program_success_ref)
+@show shader_program_success_ref[]
+MGL.glDeleteShader(vertex_shader)
+MGL.glDeleteShader(fragment_shader)
+
+vertices = MGL.GLfloat[
+-0.5f0 -0.5f0  0.0f0
+ 0.5f0 -0.5f0  0.0f0
+ 0.0f0  0.5f0  0.0f0
+]
+vertices = permutedims(vertices)
+@show vertices
+
+VAO_ref = Ref{MGL.GLuint}(0)
+MGL.glGenVertexArrays(1, VAO_ref)
+@show VAO_ref[]
+
+VBO_ref = Ref{MGL.GLuint}(0)
+MGL.glGenBuffers(1, VBO_ref)
+@show VBO_ref[]
+
+MGL.glBindVertexArray(VAO_ref[])
+
+MGL.glBindBuffer(MGL.GL_ARRAY_BUFFER, VBO_ref[])
+
+MGL.glBufferData(MGL.GL_ARRAY_BUFFER, sizeof(vertices), vertices, MGL.GL_STATIC_DRAW)
+
+MGL.glVertexAttribPointer(0, 3, MGL.GL_FLOAT, MGL.GL_FALSE, 3 * sizeof(eltype(vertices)), C_NULL)
+
+MGL.glEnableVertexAttribArray(0)
+
+MGL.glBindBuffer(MGL.GL_ARRAY_BUFFER, 0)
+
+MGL.glBindVertexArray(0)
+
+while !GLFW.WindowShouldClose(window)
+    process_input(window)
+
+    MGL.glClearColor(0.5f0, 0.5f0, 0.5f0, 1.0f0)
+    MGL.glClear(MGL.GL_COLOR_BUFFER_BIT)
+
+    MGL.glUseProgram(shader_program)
+    MGL.glBindVertexArray(VAO_ref[])
+    MGL.glDrawArrays(MGL.GL_TRIANGLES, 0, 3)
+
+    GLFW.SwapBuffers(window)
+    GLFW.PollEvents()
+end
+
+MGL.glDeleteVertexArrays(1, VAO_ref)
+MGL.glDeleteBuffers(1, VBO_ref)
+MGL.glDeleteProgram(shader_program)
+
+GLFW.DestroyWindow(window)
