c96ac9777a51632425bb9ee93464791ff124536d
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2022-04-02 22:37:06 +0530

    add text input widget (#11)

--- ui.jl
+++ ui.jl
@@ -13,6 +13,9 @@ const UI_BUTTON = UIButton()
 struct UISlider <: AbstractWidget end
 const UI_SLIDER = UISlider()
 
+struct UITextInput <: AbstractWidget end
+const UI_TEXT_INPUT = UITextInput()
+
 went_down(ended_down, half_transition_count) = (half_transition_count >= 2) || ((half_transition_count == 1) && ended_down)
 went_up(ended_down, half_transition_count) = (half_transition_count >= 2) || ((half_transition_count == 1) && !ended_down)
 
@@ -99,4 +102,41 @@ function widget(hot_widget, active_widget, widget, widget_type::UISlider, i_min,
     hot_widget = try_reset_hot_widget(hot_widget, active_widget, widget, !mouse_over_slider)
 
     return hot_widget, active_widget, value
+end
+
+function update_widget_value!(hot_widget, active_widget, widget, ::UITextInput, text_line, characters)
+    if (active_widget == widget) && (hot_widget == widget)
+        for character in characters
+            println(character)
+            if isascii(character)
+                if isprint(character)
+                    push!(text_line, character)
+                elseif character == '\b'
+                    if length(text_line) > 0
+                        pop!(text_line)
+                    end
+                end
+            end
+        end
+    end
+
+    return nothing
+end
+
+function widget!(hot_widget, active_widget, widget, widget_type::UITextInput, i_min, j_min, i_max, j_max, i_mouse, j_mouse, ended_down, half_transition_count, text_line, characters)
+    mouse_over_widget = (i_min <= i_mouse <= i_max) && (j_min <= j_mouse <= j_max)
+    mouse_went_down = went_down(ended_down, half_transition_count)
+    mouse_went_up = went_up(ended_down, half_transition_count)
+
+    hot_widget = try_set_hot_widget(hot_widget, active_widget, widget, mouse_over_widget)
+
+    active_widget = try_set_active_widget(hot_widget, active_widget, widget, mouse_over_widget && mouse_went_up)
+
+    update_widget_value!(hot_widget, active_widget, widget, widget_type, text_line, characters)
+
+    active_widget = try_reset_active_widget(hot_widget, active_widget, widget, !mouse_over_widget && mouse_went_up)
+
+    hot_widget = try_reset_hot_widget(hot_widget, active_widget, widget, !mouse_over_widget)
+
+    return hot_widget, active_widget
 end
