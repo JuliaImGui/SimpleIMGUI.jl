d5ba3072815b554616e6fb9d6119e61ac34a65d1
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2022-05-01 13:51:27 +0530

    add BoundingBox (#25)
    * add BoundingBox
    
    * pass bounding_box to widget and widget!
    
    * use BoundingBox in example

--- src/input.jl
+++ src/input.jl
@@ -8,6 +8,15 @@ struct Cursor
     j::Int
 end
 
+struct BoundingBox
+    i_min::Int
+    j_min::Int
+    i_max::Int
+    j_max::Int
+end
+
 press_button(button) = InputButton(true, button.half_transition_count + one(button.half_transition_count))
 release_button(button) = InputButton(false, button.half_transition_count + one(button.half_transition_count))
 reset(button) = InputButton(button.ended_down, zero(button.half_transition_count))
+
+is_inside(bounding_box, cursor) = (bounding_box.i_min <= cursor.i <= bounding_box.i_max) && (bounding_box.j_min <= cursor.j <= bounding_box.j_max)
