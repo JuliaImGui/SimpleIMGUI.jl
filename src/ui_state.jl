99de4a85e6dda24430655031062b36fe506c8d04
Siddharth Bhatia <sidb60200@gmail.com>
2022-05-01 13:57:09 +0530

    rename UIState to UserInteractionState, ui_state to user_interaction_state

--- src/ui_state.jl
+++ src/ui_state.jl
@@ -9,12 +9,12 @@ abstract type AbstractUIState end
 
 const NULL_WIDGET_ID = WidgetID(0, "")
 
-function do_widget!(ui_state::AbstractUIState, args...; kwargs...)
-    hot_widget, active_widget, null_widget, values = do_widget!!(ui_state.hot_widget, ui_state.active_widget, ui_state.null_widget, args...; kwargs...)
+function do_widget!(user_interaction_state::AbstractUIState, args...; kwargs...)
+    hot_widget, active_widget, null_widget, values = do_widget!!(user_interaction_state.hot_widget, user_interaction_state.active_widget, user_interaction_state.null_widget, args...; kwargs...)
 
-    ui_state.hot_widget = hot_widget
-    ui_state.active_widget = active_widget
-    ui_state.null_widget = null_widget
+    user_interaction_state.hot_widget = hot_widget
+    user_interaction_state.active_widget = active_widget
+    user_interaction_state.null_widget = null_widget
 
     return values
 end
