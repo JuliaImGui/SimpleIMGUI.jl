6f17fb7c247cdc47c5192cd5d148fce1f6e066a9
Siddharth Bhatia <sidb60200@gmail.com>
2022-05-02 19:27:52 +0530

    add went_up, went_down methods for InputButton

--- src/utils.jl
+++ src/utils.jl
@@ -1,5 +1,8 @@
 went_down(ended_down, half_transition_count) = (half_transition_count >= 2) || ((half_transition_count == 1) && ended_down)
+went_down(input_button) = went_down(input_button.ended_down, input_button.half_transition_count)
+
 went_up(ended_down, half_transition_count) = (half_transition_count >= 2) || ((half_transition_count == 1) && !ended_down)
+went_up(input_button) = went_up(input_button.ended_down, input_button.half_transition_count)
 
 function try_set_hot_widget(hot_widget, active_widget, null_widget, widget, condition)
     if (active_widget == null_widget) && condition
