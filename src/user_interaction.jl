4f5eea5a84aaac50e1a3c3c9efacf3462836507e
Siddharth Bhatia <sidb60200@gmail.com>
2022-06-27 23:26:30 +0530

    reverse order of line and file attributs in WidgetID

--- src/user_interaction.jl
+++ src/user_interaction.jl
@@ -1,8 +1,8 @@
 abstract type AbstractWidgetID end
 
 struct WidgetID <: AbstractWidgetID
-    line::Int
     file::String
+    line::Int
 end
 
 abstract type AbstractUserInteractionState end
@@ -13,7 +13,7 @@ mutable struct UserInteractionState <: AbstractUserInteractionState
     null_widget::WidgetID
 end
 
-const NULL_WIDGET_ID = WidgetID(0, "")
+const NULL_WIDGET_ID = WidgetID("", 0)
 
 function do_widget!(widget_type::AbstractWidgetType, user_interaction_state::AbstractUserInteractionState, args...; kwargs...)
     hot_widget, active_widget, null_widget, values = do_widget!!(widget_type, user_interaction_state.hot_widget, user_interaction_state.active_widget, user_interaction_state.null_widget, args...; kwargs...)
