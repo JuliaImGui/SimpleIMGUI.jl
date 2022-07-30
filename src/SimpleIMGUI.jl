a3b5d37bb1241732145161bdf2520017163b7808
Siddharth Bhatia <sidb60200@gmail.com>
2022-07-30 17:49:39 +0530

    remove color_scheme.jl

--- src/SimpleIMGUI.jl
+++ src/SimpleIMGUI.jl
@@ -3,7 +3,6 @@ module SimpleIMGUI
 import SimpleDraw as SD
 
 include("input.jl")
-include("color_scheme.jl")
 include("layout.jl")
 include("user_interaction.jl")
 include("widgets.jl")
--- src/drawing.jl
+++ src/drawing.jl
@@ -1,3 +1,21 @@
+struct ContextualColor{C}
+    neutral_color::C
+    hot_color::C
+    active_color::C
+end
+
+get_color(user_interaction_state, this_widget, color) = color
+
+function get_color(user_interaction_state, this_widget, color::ContextualColor)
+    if this_widget == user_interaction_state.active_widget
+        return color.active_color
+    elseif this_widget == user_interaction_state.hot_widget
+        return color.hot_color
+    else
+        return color.neutral_color
+    end
+end
+
 function get_num_printable_characters(text)
     num_printable = 0
 
