88b7a251ccb507607b960f1393fb32989f5740b4
Siddharth Bhatia <sidb60200@gmail.com>
2022-08-05 16:38:22 +0530

    add type parameter for line and instance in WidgetID

--- src/user_interaction.jl
+++ src/user_interaction.jl
@@ -1,17 +1,17 @@
 abstract type AbstractWidgetID end
 
-struct WidgetID{S} <: AbstractWidgetID
+struct WidgetID{S, I} <: AbstractWidgetID
     file::S
-    line::Int
-    instance::Int
+    line::I
+    instance::I
 end
 
 abstract type AbstractUserInteractionState end
 
-mutable struct UserInteractionState{S} <: AbstractUserInteractionState
-    hot_widget::WidgetID{S}
-    active_widget::WidgetID{S}
-    null_widget::WidgetID{S}
+mutable struct UserInteractionState{S, I} <: AbstractUserInteractionState
+    hot_widget::WidgetID{S, I}
+    active_widget::WidgetID{S, I}
+    null_widget::WidgetID{S, I}
 end
 
 const NULL_WIDGET_ID = WidgetID("", 0, 0)
