44de5d1133e6380840785dde2cbbb95055f4e6c2
Siddharth Bhatia <sidb60200@gmail.com>
2022-08-05 17:18:37 +0530

    use typeof(input_button) instead of hardcoding InputButton

--- src/input.jl
+++ src/input.jl
@@ -24,9 +24,9 @@ went_up(ended_down, num_transitions) = (num_transitions >= 2) || (isone(num_tran
 went_down(input_button) = went_down(input_button.ended_down, input_button.num_transitions)
 went_up(input_button) = went_up(input_button.ended_down, input_button.num_transitions)
 
-press(input_button) = InputButton(true, input_button.num_transitions + one(input_button.num_transitions))
-release(input_button) = InputButton(false, input_button.num_transitions + one(input_button.num_transitions))
-reset(input_button) = InputButton(input_button.ended_down, zero(input_button.num_transitions))
+press(input_button) = typeof(input_button)(true, input_button.num_transitions + one(input_button.num_transitions))
+release(input_button) = typeof(input_button)(false, input_button.num_transitions + one(input_button.num_transitions))
+reset(input_button) = typeof(input_button)(input_button.ended_down, zero(input_button.num_transitions))
 
 function reset!(user_input_state)
     user_input_state.key_escape = reset(user_input_state.key_escape)
