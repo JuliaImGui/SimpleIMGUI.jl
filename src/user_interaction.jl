ce55d5675d1a026f7fa96fc5f3466332c1914321
Siddharth Bhatia <sidb60200@gmail.com>
2022-08-05 16:31:12 +0530

    add type parameter to file name in WidgetID

--- src/user_interaction.jl
+++ src/user_interaction.jl
@@ -1,17 +1,17 @@
 abstract type AbstractWidgetID end
 
-struct WidgetID <: AbstractWidgetID
-    file::String
+struct WidgetID{S} <: AbstractWidgetID
+    file::S
     line::Int
     instance::Int
 end
 
 abstract type AbstractUserInteractionState end
 
-mutable struct UserInteractionState <: AbstractUserInteractionState
-    hot_widget::WidgetID
-    active_widget::WidgetID
-    null_widget::WidgetID
+mutable struct UserInteractionState{S} <: AbstractUserInteractionState
+    hot_widget::WidgetID{S}
+    active_widget::WidgetID{S}
+    null_widget::WidgetID{S}
 end
 
 const NULL_WIDGET_ID = WidgetID("", 0, 0)
