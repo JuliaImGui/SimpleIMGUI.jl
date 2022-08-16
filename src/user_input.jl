859b7ca72a468885d64d8ba1f7d8300822f280ad
Siddharth Bhatia <sidb60200@gmail.com>
2022-08-16 18:52:43 +0530

    add lower level press, release, and reset methods

--- src/user_input.jl
+++ src/user_input.jl
@@ -18,9 +18,13 @@ went_up(ended_down, num_transitions) = (num_transitions >= 2) || (isone(num_tran
 went_down(input_button) = went_down(input_button.ended_down, input_button.num_transitions)
 went_up(input_button) = went_up(input_button.ended_down, input_button.num_transitions)
 
-press(input_button) = typeof(input_button)(true, input_button.num_transitions + one(input_button.num_transitions))
-release(input_button) = typeof(input_button)(false, input_button.num_transitions + one(input_button.num_transitions))
-reset(input_button) = typeof(input_button)(input_button.ended_down, zero(input_button.num_transitions))
+press(ended_down, num_transitions) = (true, num_transitions + one(num_transitions))
+release(ended_down, num_transitions) = (false, num_transitions + one(num_transitions))
+reset(ended_down, num_transitions) = (ended_down, zero(num_transitions))
+
+press(input_button) = typeof(input_button)(press(input_button.ended_down, input_button.num_transitions)...)
+release(input_button) = typeof(input_button)(release(input_button.ended_down, input_button.num_transitions)...)
+reset(input_button) = typeof(input_button)(reset(input_button.ended_down, input_button.num_transitions)...)
 
 function reset!(user_input_state)
     for (i, input_button) in enumerate(user_input_state.keyboard_buttons)
