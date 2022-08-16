c06a199cd4ebab9e55afa6e934ffb9941d7eb745
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2022-08-16 18:54:35 +0530

    change type parameters of UserInputState and add lower level methods for press, release, reset (#61)
    * parameterize InputButton{I2} and Char in UserInputState
    
    * add lower level press, release, and reset methods

--- src/user_input.jl
+++ src/user_input.jl
@@ -5,11 +5,11 @@ end
 
 abstract type AbstractUserInputState end
 
-mutable struct UserInputState{I1, I2} <: AbstractUserInputState
-    cursor::SD.Point{I1}
-    keyboard_buttons::Vector{InputButton{I2}}
-    mouse_buttons::Vector{InputButton{I2}}
-    characters::Vector{Char}
+mutable struct UserInputState{I, T, C} <: AbstractUserInputState
+    cursor::SD.Point{I}
+    keyboard_buttons::Vector{T}
+    mouse_buttons::Vector{T}
+    characters::Vector{C}
 end
 
 went_down(ended_down, num_transitions) = (num_transitions >= 2) || (isone(num_transitions) && ended_down)
@@ -18,9 +18,13 @@ went_up(ended_down, num_transitions) = (num_transitions >= 2) || (isone(num_tran
 went_down(input_button) = went_down(input_button.ended_down, input_button.num_transitions)
 went_up(input_button) = went_up(input_button.ended_down, input_button.num_transitions)
 
-press(input_button) = typeof(input_button)(true, input_button.num_transitions + one(input_button.num_transitions))
-release(input_button) = typeof(input_button)(false, input_button.num_transitions + one(input_button.num_transitions))
-reset(input_button) = typeof(input_button)(input_button.ended_down, zero(input_button.num_transitions))
+press(ended_down, num_transitions) = (true, num_transitions + one(num_transitions))
+release(ended_down, num_transitions) = (false, num_transitions + one(num_transitions))
+reset(ended_down, num_transitions) = (ended_down, zero(num_transitions))
+
+press(input_button) = typeof(input_button)(press(input_button.ended_down, input_button.num_transitions)...)
+release(input_button) = typeof(input_button)(release(input_button.ended_down, input_button.num_transitions)...)
+reset(input_button) = typeof(input_button)(reset(input_button.ended_down, input_button.num_transitions)...)
 
 function reset!(user_input_state)
     for (i, input_button) in enumerate(user_input_state.keyboard_buttons)
