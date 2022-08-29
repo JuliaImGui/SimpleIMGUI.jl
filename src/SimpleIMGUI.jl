5d092b174f6007046795309326f648281cb0cfa5
Siddharth Bhatia <sidb60200@gmail.com>
2022-08-29 23:22:43 +0530

    rename widgets.jl to widget_core.jl

--- src/SimpleIMGUI.jl
+++ src/SimpleIMGUI.jl
@@ -5,7 +5,7 @@ import SimpleDraw as SD
 include("user_input.jl")
 include("layout.jl")
 include("user_interaction.jl")
-include("widgets.jl")
+include("widget_core.jl")
 include("widget_utils.jl")
 include("drawing.jl")
 
