69509d1f47c81094c1554c72254bf55107d05f45
Siddharth Bhatia <sidb60200@gmail.com>
2023-01-14 01:12:16 -0600

    remove one layer of do_widget! methods

--- src/widget_utils.jl
+++ src/widget_utils.jl
@@ -8,147 +8,7 @@ mutable struct UIContext{S, I1, I2, I3, A, F, C}
 end
 
 #####
-##### helper methods for bundled arguments for methods in widget_core.jl
-#####
-
-function do_widget!(
-        widget_type::Union{Button, Text, Image},
-        user_interaction_state,
-        this_widget,
-        cursor,
-        input_button,
-        widget_bounding_box,
-    )
-
-    hot_widget, active_widget, null_widget, new_widget_value = do_widget!!(
-        widget_type,
-        user_interaction_state.hot_widget,
-        user_interaction_state.active_widget,
-        user_interaction_state.null_widget,
-        this_widget,
-        cursor.i,
-        cursor.j,
-        input_button.ended_down,
-        input_button.num_transitions,
-        SD.get_i_min(widget_bounding_box),
-        SD.get_j_min(widget_bounding_box),
-        SD.get_i_max(widget_bounding_box),
-        SD.get_j_max(widget_bounding_box),
-    )
-
-    user_interaction_state.hot_widget = hot_widget
-    user_interaction_state.active_widget = active_widget
-    user_interaction_state.null_widget = null_widget
-
-    return new_widget_value
-end
-
-function do_widget!(
-        widget_type::Union{CheckBox, RadioButton, DropDown},
-        user_interaction_state,
-        this_widget,
-        widget_value,
-        cursor,
-        input_button,
-        widget_bounding_box,
-    )
-
-    hot_widget, active_widget, null_widget, new_widget_value = do_widget!!(
-        widget_type,
-        user_interaction_state.hot_widget,
-        user_interaction_state.active_widget,
-        user_interaction_state.null_widget,
-        this_widget,
-        widget_value,
-        cursor.i,
-        cursor.j,
-        input_button.ended_down,
-        input_button.num_transitions,
-        SD.get_i_min(widget_bounding_box),
-        SD.get_j_min(widget_bounding_box),
-        SD.get_i_max(widget_bounding_box),
-        SD.get_j_max(widget_bounding_box),
-    )
-
-    user_interaction_state.hot_widget = hot_widget
-    user_interaction_state.active_widget = active_widget
-    user_interaction_state.null_widget = null_widget
-
-    return new_widget_value
-end
-
-function do_widget!(
-        widget_type::TextBox,
-        user_interaction_state,
-        this_widget,
-        widget_value,
-        cursor,
-        input_button,
-        characters,
-        widget_bounding_box,
-    )
-
-    hot_widget, active_widget, null_widget, new_widget_value = do_widget!!(
-        widget_type,
-        user_interaction_state.hot_widget,
-        user_interaction_state.active_widget,
-        user_interaction_state.null_widget,
-        this_widget,
-        widget_value,
-        cursor.i,
-        cursor.j,
-        input_button.ended_down,
-        input_button.num_transitions,
-        characters,
-        SD.get_i_min(widget_bounding_box),
-        SD.get_j_min(widget_bounding_box),
-        SD.get_i_max(widget_bounding_box),
-        SD.get_j_max(widget_bounding_box),
-    )
-
-    user_interaction_state.hot_widget = hot_widget
-    user_interaction_state.active_widget = active_widget
-    user_interaction_state.null_widget = null_widget
-
-    return new_widget_value
-end
-
-function do_widget!(
-        widget_type::Slider,
-        user_interaction_state,
-        this_widget,
-        widget_value,
-        cursor,
-        input_button,
-        widget_bounding_box,
-    )
-
-    hot_widget, active_widget, null_widget, new_widget_value = do_widget!!(
-        widget_type,
-        user_interaction_state.hot_widget,
-        user_interaction_state.active_widget,
-        user_interaction_state.null_widget,
-        this_widget,
-        widget_value...,
-        cursor.i,
-        cursor.j,
-        input_button.ended_down,
-        input_button.num_transitions,
-        SD.get_i_min(widget_bounding_box),
-        SD.get_j_min(widget_bounding_box),
-        SD.get_i_max(widget_bounding_box),
-        SD.get_j_max(widget_bounding_box),
-    )
-
-    user_interaction_state.hot_widget = hot_widget
-    user_interaction_state.active_widget = active_widget
-    user_interaction_state.null_widget = null_widget
-
-    return new_widget_value
-end
-
-#####
-##### helper methods that also do the widget layouting and drawing
+##### helper methods that bundle some of the arguments for methods in widget_core.jl and also do widget layouting and drawing
 #####
 
 function do_widget!(
@@ -175,7 +35,25 @@ function do_widget!(
     widget_bounding_box = get_alignment_bounding_box(layout.reference_bounding_box, alignment, padding, widget_height, widget_width)
     layout.reference_bounding_box = widget_bounding_box
 
-    widget_value = do_widget!(widget_type, user_interaction_state, this_widget, cursor, input_button, widget_bounding_box)
+    hot_widget, active_widget, null_widget, widget_value = do_widget!!(
+        widget_type,
+        user_interaction_state.hot_widget,
+        user_interaction_state.active_widget,
+        user_interaction_state.null_widget,
+        this_widget,
+        cursor.i,
+        cursor.j,
+        input_button.ended_down,
+        input_button.num_transitions,
+        SD.get_i_min(widget_bounding_box),
+        SD.get_j_min(widget_bounding_box),
+        SD.get_i_max(widget_bounding_box),
+        SD.get_j_max(widget_bounding_box),
+    )
+
+    user_interaction_state.hot_widget = hot_widget
+    user_interaction_state.active_widget = active_widget
+    user_interaction_state.null_widget = null_widget
 
     draw_widget!(image, widget_type, widget_bounding_box, user_interaction_state, this_widget, text, font, content_alignment, content_padding, background_color, border_color, text_color)
 
@@ -208,7 +86,26 @@ function do_widget!(
     widget_bounding_box = get_alignment_bounding_box(layout.reference_bounding_box, alignment, padding, widget_height, widget_width)
     layout.reference_bounding_box = widget_bounding_box
 
-    widget_value = do_widget!(widget_type, user_interaction_state, this_widget, widget_value, cursor, input_button, widget_bounding_box)
+    hot_widget, active_widget, null_widget, widget_value = do_widget!!(
+        widget_type,
+        user_interaction_state.hot_widget,
+        user_interaction_state.active_widget,
+        user_interaction_state.null_widget,
+        this_widget,
+        widget_value,
+        cursor.i,
+        cursor.j,
+        input_button.ended_down,
+        input_button.num_transitions,
+        SD.get_i_min(widget_bounding_box),
+        SD.get_j_min(widget_bounding_box),
+        SD.get_i_max(widget_bounding_box),
+        SD.get_j_max(widget_bounding_box),
+    )
+
+    user_interaction_state.hot_widget = hot_widget
+    user_interaction_state.active_widget = active_widget
+    user_interaction_state.null_widget = null_widget
 
     draw_widget!(image, widget_type, widget_bounding_box, user_interaction_state, this_widget, widget_value, content_alignment, content_padding, text, font, background_color, border_color, text_color, indicator_color)
 
@@ -240,7 +137,27 @@ function do_widget!(
     widget_bounding_box = get_alignment_bounding_box(layout.reference_bounding_box, alignment, padding, widget_height, widget_width)
     layout.reference_bounding_box = widget_bounding_box
 
-    widget_value = do_widget!(widget_type, user_interaction_state, this_widget, widget_value, cursor, input_button, characters, widget_bounding_box)
+    hot_widget, active_widget, null_widget, widget_value = do_widget!!(
+        widget_type,
+        user_interaction_state.hot_widget,
+        user_interaction_state.active_widget,
+        user_interaction_state.null_widget,
+        this_widget,
+        widget_value,
+        cursor.i,
+        cursor.j,
+        input_button.ended_down,
+        input_button.num_transitions,
+        characters,
+        SD.get_i_min(widget_bounding_box),
+        SD.get_j_min(widget_bounding_box),
+        SD.get_i_max(widget_bounding_box),
+        SD.get_j_max(widget_bounding_box),
+    )
+
+    user_interaction_state.hot_widget = hot_widget
+    user_interaction_state.active_widget = active_widget
+    user_interaction_state.null_widget = null_widget
 
     draw_widget!(image, widget_type, widget_bounding_box, user_interaction_state, this_widget, widget_value, content_alignment, content_padding, font, background_color, border_color, text_color)
 
@@ -268,7 +185,26 @@ function do_widget!(
     widget_bounding_box = get_alignment_bounding_box(layout.reference_bounding_box, alignment, padding, widget_height, widget_width)
     layout.reference_bounding_box = widget_bounding_box
 
-    widget_value = do_widget!(widget_type, user_interaction_state, this_widget, widget_value, cursor, input_button, widget_bounding_box)
+    hot_widget, active_widget, null_widget, widget_value = do_widget!!(
+        widget_type,
+        user_interaction_state.hot_widget,
+        user_interaction_state.active_widget,
+        user_interaction_state.null_widget,
+        this_widget,
+        widget_value...,
+        cursor.i,
+        cursor.j,
+        input_button.ended_down,
+        input_button.num_transitions,
+        SD.get_i_min(widget_bounding_box),
+        SD.get_j_min(widget_bounding_box),
+        SD.get_i_max(widget_bounding_box),
+        SD.get_j_max(widget_bounding_box),
+    )
+
+    user_interaction_state.hot_widget = hot_widget
+    user_interaction_state.active_widget = active_widget
+    user_interaction_state.null_widget = null_widget
 
     draw_widget!(image, widget_type, widget_bounding_box, user_interaction_state, this_widget, widget_value, background_color, border_color, indicator_color)
 
@@ -296,7 +232,25 @@ function do_widget!(
     widget_bounding_box = get_alignment_bounding_box(layout.reference_bounding_box, alignment, padding, widget_height, widget_width)
     layout.reference_bounding_box = widget_bounding_box
 
-    widget_value = do_widget!(widget_type, user_interaction_state, this_widget, cursor, input_button, widget_bounding_box)
+    hot_widget, active_widget, null_widget, widget_value = do_widget!!(
+        widget_type,
+        user_interaction_state.hot_widget,
+        user_interaction_state.active_widget,
+        user_interaction_state.null_widget,
+        this_widget,
+        cursor.i,
+        cursor.j,
+        input_button.ended_down,
+        input_button.num_transitions,
+        SD.get_i_min(widget_bounding_box),
+        SD.get_j_min(widget_bounding_box),
+        SD.get_i_max(widget_bounding_box),
+        SD.get_j_max(widget_bounding_box),
+    )
+
+    user_interaction_state.hot_widget = hot_widget
+    user_interaction_state.active_widget = active_widget
+    user_interaction_state.null_widget = null_widget
 
     draw_widget!(image, widget_type, widget_bounding_box, user_interaction_state, this_widget, content, content_alignment, content_padding, border_color)
 
